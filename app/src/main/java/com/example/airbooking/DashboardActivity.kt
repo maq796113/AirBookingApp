0ef9feed715116d58133eeb55e5f815638e26363
Muhammad Abdullah Qureshi <maq796113@gmail.com>
2023-11-21 20:01:34 +0500

    update 11/21/2023

--- app/src/main/java/com/example/airbooking/DashboardActivity.kt
+++ app/src/main/java/com/example/airbooking/DashboardActivity.kt
@@ -2,30 +2,37 @@ package com.example.airbooking
 
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat.getString
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
 import com.example.airbooking.data.User
 import com.example.airbooking.ui.theme.AirBookingTheme
 import com.example.airbooking.viewModel.UserViewModel
+import com.google.relay.compose.RowScopeInstanceImpl.align
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -36,7 +43,8 @@ class DashboardActivity : ComponentActivity() {
             AirBookingTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier
+                        .fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     DashboardLayout()
@@ -69,17 +77,20 @@ fun UserProfilePic(
                 AsyncImage(
                     Uri.parse(user.profilePictureUri),
                     contentScale = ContentScale.Crop,
-                    contentDescription = null,
-                    modifier = Modifier
-                        .clip(CircleShape)
-                        .matchParentSize()
+                    contentDescription = null
                 )
             }else{
-                Text(
-                    text = "Null",
-                    fontWeight = FontWeight.Bold,
+                AsyncImage(
+                    model = getString(LocalContext.current, R.string.man_silhouette),
+                    contentDescription = "Man's Silhoutte/Facebook Default Image",
+                    contentScale = ContentScale.Crop,
+                    alignment=Alignment.Center,
                     modifier = Modifier
-                        .matchParentSize()
+                        .width(Dp(125.5f))
+                        .padding(10.dp)
+                        .aspectRatio(1920f / 1712f)
+                        .clip(CircleShape)
+
                 )
             }
         }
@@ -90,6 +101,10 @@ fun UserProfilePic(
 fun DashboardLayout(
     viewModel: UserViewModel = hiltViewModel()
 ) {
-    UserProfilePic(user = viewModel.session.getActiveSession()?.user)
-
+    Log.d("Active Session", viewModel.session.getActiveSession()?.user.toString())
+    Column(
+        modifier = Modifier.align(Alignment.Top)
+    ) {
+        UserProfilePic(user = viewModel.session.getActiveSession()?.user)
+    }
 }
